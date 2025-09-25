8eaf14474c6c6e800a658fdac50cb4760827fb97
Amorim_gk.12 <gabriel.amorim.etec@gmail.com>
2025-09-25 11:04:35 +0000

    Colocando Imgs

--- App.jsx
+++ App.jsx
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
--- pages/roupas.jsx
+++ pages/roupas.jsx
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Pressable, ScrollView } from 'react-native';
+import { StyleSheet, Text, Pressable, ScrollView, Image } from 'react-native';
+import { View } from 'react-native-web';
+
+const image1 = {uri: 'https://i.pinimg.com/474x/26/29/ee/2629ee107e81b6c820febbf7eb29d8fa.jpg'};
 
 export default function Roupas() {
   return ( 
     <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.containerMasc}>
+          <Text style={styles.titulo}>Roupas Masculinas</Text>
+          <View>
+            <Image source={ image }></Image>
+            <Image></Image>
+            <Image></Image>
+            <Image></Image>
+          </View>
+        </View>
+        <View style={styles.containerFem}>
 
+        </View>
     </ScrollView>      
   );
 }
@@ -17,13 +30,21 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     flexDirection: 'column',
   },
-  botao: {
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "yellow",
-    width: 200,
-    height: 100,
-    borderRadius: 20,
-    marginBottom: 15,
-  }
+  containerMasc: {
+    backgroundColor: '#ADD8E6',
+    width: '100%',
+    height: '45%',
+    marginBottom: 20,
+  },
+  containerFem: {
+    backgroundColor: '#FFB6C1',
+    width: '100%',
+    height: '45%',
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#f5efdb',
+  },
 });
